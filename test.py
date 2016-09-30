3c2c3a5d067eb2fd3d598337fe2317f088dcb40e
Maxim Khlamov <mkhlamov@gmail.com>
2016-09-30 16:47:33 +0300

    Added .idea to gitignore Added test file

---
+++ test.py
@@ -0,0 +1,15 @@
+__author__ = 'student'
+
+primes = [2, 3, 5, 7, 11, 13]
+Rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+
+# a = []
+# for i in range(1,10):
+#      a.append(i**2)
+# print(a)
+
+a = [str(i) for i in range(1, 10) if i % 2 == 0]
+
+print(a)
+
+
