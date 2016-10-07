d80b4793de2ffed89795f248b46cf7a89e2eb4fd
Maxim Khlamov <mkhlamov@gmail.com>
2016-10-07 15:48:32 +0300

    some tasks

---
+++ 2.2.py
@@ -0,0 +1,5 @@
+__author__ = 'student'
+A = [1,2,3,2,1]
+b = [A[i-1] for i in range(len(A))]
+print(b)
+
---
+++ 2.3.py
@@ -0,0 +1,5 @@
+__author__ = 'student'
+A =[1,2,2,3,3,3]
+for x in A:
+    if A.count(x) == 1:
+        print(x)
